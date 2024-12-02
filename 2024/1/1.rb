678d729ad884cc31c32d8ba85bbc2dafaec121e7
John-Patrick Di Cosola <john@timedochealth.com>
2024-12-01 21:58:00 -0600

    day 1 part 2

--- 2024/1/1.rb
+++ 2024/1/1.rb
@@ -1,15 +1,20 @@
-col1 = []
-col2 = []
+def load_data(file_name)
+  col1 = []
+  col2 = []
+  IO.read(file_name).split("\n").each do |line|
+    col1.push(line.split(" ")[0].to_i)
+    col2.push(line.split(" ")[1].to_i)
+  end
+  return col1, col2
+end
 
 input_file = ARGV[0] || "./input.txt"
 
-IO.read(input_file).split("\n").each do |line|
-  col1.push(line.split(" ")[0].to_i)
-  col2.push(line.split(" ")[1].to_i)
-end
+data = load_data(input_file)
+col1 = data[0].sort
+col2 = data[1].sort
 
-col1.sort!
-col2.sort!
+# Part 1
 
 dist = 0
 
@@ -18,3 +23,16 @@ while (col1.length != 0)
 end
 
 puts "Distance: #{dist}"
+
+# Part 2
+
+similarity = 0
+
+col1 = data[0]
+col2 = data[1]
+
+for i in 0..(col1.length - 1)
+  similarity += (col2.count { |x| x == col1[i] }) * col1[i]
+end
+
+puts "Similarity Score: #{similarity}"
