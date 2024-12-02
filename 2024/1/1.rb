7cc20ffb36022d80536c9055e3d481407768293d
John-Patrick Di Cosola <john@timedochealth.com>
2024-12-01 21:36:03 -0600

    day 1

---
+++ 2024/1/1.rb
@@ -0,0 +1,21 @@
+col1 = []
+col2 = []
+
+# Read input from ARGV
+input_file = ARGV[0] || "./input.txt"
+
+IO.read(input_file).split("\n").each do |line|
+  col1.push(line.split(" ")[0].to_i)
+  col2.push(line.split(" ")[1].to_i)
+end
+
+col1.sort!
+col2.sort!
+
+dist = 0
+
+while (col1.length != 0)
+  dist += (col1.shift - col2.shift).abs
+end
+
+puts "Distance: #{dist}"
