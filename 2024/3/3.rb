5078c72e2b0b9b511ad58158f2983fa395707fdf
John-Patrick Di Cosola <john@timedochealth.com>
2024-12-03 21:50:50 -0600

    day 3 part 2

--- 2024/3/3.rb
+++ 2024/3/3.rb
@@ -1,11 +1,41 @@
+require "pry"
+
 input_file = ARGV[0] || "./input.txt"
 data = IO.read(input_file)
-matches = data.scan(/mul\((\d{1,3})\,(\d{1,3})\)/)
 
+# The do() instruction enables future mul instructions.
+# The don't() instruction disables future mul instructions.
+
+pointer = 0
 sum = 0
+processing_enabled = false
 
-matches.each do |m|
-  sum += m[0].to_i * m[1].to_i
+processing_enabled = true
+
+while (pointer < data.length)
+  next_do = data[pointer..-1].index("do()")
+  next_dont = data[pointer..-1].index("don't()")
+  next_instruction = [next_do, next_dont].compact.min
+
+  end_idx = next_instruction.nil? ? data.length : pointer + next_instruction
+  segment = data[pointer...end_idx]
+
+  if processing_enabled
+    matches = segment.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+    sum += matches.map { |m| m[0].to_i * m[1].to_i }.sum
+  end
+
+  pointer = end_idx
+
+  if next_instruction
+    if data[pointer..].start_with?("do()")
+      processing_enabled = true
+      pointer += 4
+    elsif data[pointer..].start_with?("don't()")
+      processing_enabled = false
+      pointer += 7
+    end
+  end
 end
 
 puts "Sum: #{sum}"
